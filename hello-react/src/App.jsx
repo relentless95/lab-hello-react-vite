ea386f527fe4cf0e9d9ddecbe4d004ff5632f5ff
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-03-29 09:51:39 +0200

    re-did the styling

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -15,44 +15,49 @@ function App() {
 
   return (
     <div className="App">
-      <div className="first-container">
+      <div className="main-container">
+        <div className="first-container">
+          <nav className="second-container">
+            <div class="logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div class="menu">
+              <img src={menu} alt="menu" />
+            </div>
+          </nav>
+          <div className="larger-box">
+            <span>
+              <h1>Say hello to ReactJS</h1>
+            </span>
+            <p>
+              You will learn how to use the most popular frontend library, and
+              become a super Ninja developer.
+            </p>
+            </div>
+            <button className="btn">Awesome!</button>
+          
+        </div>
         <div className="second-container">
-          <div class="logo">
-            <img src={logo} alt="logo" />
+          <div className="box">
+            <img src={icon1} alt="image1" />
+            <h1>Declaraive</h1>
+            <p>React makes it painless to create interactive UIs.</p>
           </div>
-          <div class="menu">
-            <img src={menu} alt="menu" />
+          <div className="box">
+            <img src={icon2} alt="image2" />
+            <h1>Components</h1>
+            <p>Build encapsulaed components that manage their state.</p>
           </div>
-        </div>
-        <span>
-          <h1>Say hello to ReactJS</h1>
-        </span>
-        <p>
-          You will learn how to use the most popular frontend library, and
-          become a super Ninja developer.
-        </p>
-        <button className="btn">Awesome!</button>
-      </div>
-      <div className="second-container">
-        <div className="box">
-          <img src={icon1} alt="image1" />
-          <h1>Declaraive</h1>
-          <p>React makes it painless to create interactive UIs.</p>
-        </div>
-        <div className="box">
-          <img src={icon2} alt="image2" />
-          <h1>Components</h1>
-          <p>Build encapsulaed components that manage their state.</p>
-        </div>
-        <div className="box">
-          <img src={icon2} alt="image2" />
-          <h1>Single-Way</h1>
-          <p>A set of immutable values are passed to the components.</p>
-        </div>
-        <div className="box">
-          <img src={icon3} alt="image3" />
-          <h1>JSX</h1>
-          <p>Statically-typed designed to run on moden browsers.</p>
+          <div className="box">
+            <img src={icon2} alt="image2" />
+            <h1>Single-Way</h1>
+            <p>A set of immutable values are passed to the components.</p>
+          </div>
+          <div className="box">
+            <img src={icon3} alt="image3" />
+            <h1>JSX</h1>
+            <p>Statically-typed designed to run on moden browsers.</p>
+          </div>
         </div>
       </div>
     </div>
